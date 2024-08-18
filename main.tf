d075254035cf30fa6a41a9c8ee08752a44239977
gollapalliphaneendra <gollapalli.phani@gmail.com>
2024-08-19 00:28:47 +0530

    second Commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+    region = "ap-south-1"
+  
+}
+
+resource "aws_instance" "terraform-instance" {
+    ami = "ami-0ad21ae1d0696ad58"
+    instance_type = "t2.micro"
+  
+   tags = {
+     Name = " Ec2 Instance"
+    }
+}
+
+
+resource "aws_s3_bucket" "devansh" {
+  bucket = "devansh1231" 
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform_lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}  
